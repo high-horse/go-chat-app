74f2236393d5c84322e9e4b46d1f447864e5073b
Akhilesh Adhikari <akhileshadhikaria1@gmail.com>
2024-05-06 16:37:49 +0545

    frent

--- attempt-2/main.go
+++ attempt-2/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("hello world")
+	setupApi()
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func setupApi() {
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
 }
